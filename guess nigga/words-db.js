a655b817e26f872efae06318fa97473b7b5ac5c1
kebinfr1 <144445531+kebinfr1@users.noreply.github.com>
2023-09-16 14:04:47 +0800

    Add files via upload
    test

---
+++ guess nigga/words-db.js
@@ -0,0 +1,28 @@
+let wordList = [
+    {
+        word: "nigga",
+        hint: "black man living in nigeria"
+    },
+    {
+        word: "igothoooo",
+        hint: "CALLINNNN"
+    },
+    {
+        word: "github",
+        hint: "code hosting platform"
+    },
+    {
+        word: "png",
+        hint: "a image file format"
+    },
+    
+    {
+        word: "mobile",
+        hint: "an electronic device"
+    },
+    {
+        word: "java",
+        hint: "programming language"
+    },
+
+]
